b5dec73342a256ee62aa4398f8351dfafbd8728e
G <gegigson.cpp.edu@gmail.com>
2024-12-03 20:20:31 -0800

    added javadoc program description

--- prj_docs/project_code/volunteerLog.java
+++ prj_docs/project_code/volunteerLog.java
@@ -1,3 +1,8 @@
+/**
+ *  volunteerLog.Java is a file intended to log a volunteer's 
+ * hours, tasks, and contact information
+ * @author Joseline Ly and Garrison Gibson
+ */
 public class volunteerLog
 {
 	public static void main(String[] args) {
